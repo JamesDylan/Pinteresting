07dabceec002f79e5bab20181aa2c7bda5212df5
James Scholz <jamesscholz@outlook.com>
2013-11-23 19:31:46 +1300

    Added Devise and config changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Pinteresting::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  #### NOTE #### Set this to your actual email
+  config.action_mailer.default_url_options = { :host => 'swapshop-nz.herokuapp.com' }
 end
